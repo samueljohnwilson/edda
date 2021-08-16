34c3591e7696c7c868670960e96229d82f12446f
Sam <s.wilson@echosec.net>
2021-08-16 16:58:02 -0700

    Add enums

---
+++ src/Enums.ts
@@ -0,0 +1,12 @@
+export enum Races {
+  HUMAN = 'human',
+  DWARF = 'dwarf',
+  ELF = 'elf',
+  HALF_ELF = 'halfElf',
+  HALFLING = 'halfling',
+  FIRBOLG = 'firbolg',
+}
+
+export enum Pages {
+  RACES = 'Races',
+}
