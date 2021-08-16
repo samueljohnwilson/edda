296fbd467077f4aeaefd949b43628a6ebbf59e43
Sam <s.wilson@echosec.net>
2021-08-16 16:57:55 -0700

    Add additional types

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,9 @@
+import { Races } from './Enums';
+
+/**
+ * Layout types and interfaces.
+ */
+
 export interface BaseCardInterface {
   title: string;
   paragraphs: string[];
@@ -10,3 +16,15 @@ export interface BaseArticleDataInterface {
   image: string;
   cards: BaseCardInterface[];
 }
+
+/**
+ * Race types and interfaces.
+ */
+
+export interface ListItem {
+  name: string;
+  value: Races;
+  data: BaseArticleDataInterface;
+}
+
+export type List = Record<string, ListItem>;
