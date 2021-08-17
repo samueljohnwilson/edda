dbb77653b19a57c8d4b10e8d984d8a51b64f5270
Sam <s.wilson@echosec.net>
2021-08-17 15:08:41 -0700

    Add timeline types

--- src/types.ts
+++ src/types.ts
@@ -1,30 +1,64 @@
-import { Races } from './Enums';
+// Layout types and interfaces.
 
 /**
- * Layout types and interfaces.
+ * Interface for a single article entry.
  */
-
-export interface BaseCardInterface {
+export interface ArticleEntryInterface {
   title: string;
   paragraphs: string[];
 }
 
+/**
+ * Interface for the data used in a BaseArticle component.
+ */
 export interface BaseArticleDataInterface {
   title: string;
   quote: string;
   attribution: string;
   image: string;
-  cards: BaseCardInterface[];
+  isImageHorizontal: boolean;
+  entries: ArticleEntryInterface[];
 }
 
 /**
- * Race types and interfaces.
+ * Interface for the data and metadata used in
+ * a BaseArticle component.
  */
-
-export interface ListItem {
+export interface BaseArticleInterface {
   name: string;
-  value: Races;
+  value: string;
   data: BaseArticleDataInterface;
 }
 
-export type List = Record<string, ListItem>;
+/**
+ * Interface for a single timeline entry.
+ */
+export interface TimelineEntryInterface {
+  title: string;
+  text: string;
+  icon: string;
+}
+
+/**
+ * Interface for a page that utilizes
+ * the TimelineArticle component.
+ */
+export interface TimelineArticleDataInterface {
+  title: string;
+  events: TimelineEntryInterface[];
+}
+
+/**
+ * Interface for the data and metadata used in
+ * a TimelineArticle component.
+ */
+export interface TimelineArticleInterface {
+  name: string;
+  value: string;
+  data: TimelineArticleDataInterface;
+}
+
+/**
+ * Type designating a list of articles.
+ */
+export type ArticleList = Record<string, BaseArticleInterface>;
