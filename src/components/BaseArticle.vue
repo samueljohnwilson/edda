ff0ed4f43628130e0bf7550238b2e4c90fa454d1
Sam <s.wilson@echosec.net>
2021-09-06 20:34:31 -0700

    Refactor BaseArticle

--- src/components/BaseArticle.vue
+++ src/components/BaseArticle.vue
@@ -17,25 +17,59 @@
           :height="data.isImageHorizontal ? 400 : 600"
         />
       </transition>
-      <a
-        style="color: grey; font-size: 0.8rem"
-        class="img-source"
-        :href="data.imageSource"
-        >Image Source</a
-      >
+      <p class="img-source" style="color: grey">
+        {{ data.imageTitle }} —
+        <a style="color: grey" :href="data.imageSource" target="_blank">
+          Image Source
+        </a>
+      </p>
       <v-card-text>
         <blockquote v-html="data.quote" />
-        <v-card-text class="attribution">- {{ data.attribution }}</v-card-text>
+        <v-card-text class="attribution">{{ data.attribution }}</v-card-text>
       </v-card-text>
-      <div v-for="entry in data.entries" :key="`${entry.title}`">
-        <v-card-title>{{ entry.title }}</v-card-title>
-        <v-card-text
-          v-for="(paragraph, index) in entry.paragraphs"
-          v-html="paragraph"
-          :key="`${entry.title}-${index}`"
+      <v-row>
+        <v-col :cols="!data.sidebar || $vuetify.breakpoint.smAndDown ? 12 : 8">
+          <div v-for="entry in data.entries" :key="entry.title">
+            <v-card-title>{{ entry.title }}</v-card-title>
+            <v-card-text
+              v-for="(paragraph, index) in entry.paragraphs"
+              v-html="paragraph"
+              :key="`${entry.title}-${index}`"
+            >
+            </v-card-text>
+          </div>
+        </v-col>
+        <v-col
+          v-if="data.sidebar"
+          :cols="$vuetify.breakpoint.smAndDown ? 12 : 4"
         >
-        </v-card-text>
-      </div>
+          <v-card class="sidebar" style="padding: 10px">
+            <div
+              v-for="(el, sidebarIndex) in data.sidebar"
+              :key="`sidebar-${sidebarIndex}`"
+            >
+              <div v-if="el.image">
+                <v-img :src="el.image" height="400" />
+                <p class="img-source">
+                  {{ el.description }} —
+                  <a
+                    style="color: black"
+                    :href="data.imageSource"
+                    target="_blank"
+                  >
+                    Image Source
+                  </a>
+                </p>
+              </div>
+              <div v-else>
+                <v-card-text>
+                  {{ el.paragraph }}
+                </v-card-text>
+              </div>
+            </div>
+          </v-card>
+        </v-col>
+      </v-row>
       <Footer />
     </v-card>
   </v-container>
@@ -62,3 +96,14 @@ export default class BaseArticle extends Vue {
   private readonly data!: BaseArticleDataInterface;
 }
 </script>
+
+<style lang="less" scoped>
+.img-source {
+  padding-top: 2px;
+
+  &,
+  a {
+    font-size: 0.8rem;
+  }
+}
+</style>
