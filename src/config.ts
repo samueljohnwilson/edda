ac73df99a74ec01a0f397c8105f15d48bffac1ce
Sam <s.wilson@echosec.net>
2022-08-28 18:44:35 -0700

    Add env vars

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,10 @@
 require('dotenv').config();
+// @ts-expect-error
+import * as path from 'path'
+
+// @ts-expect-error
+console.log(path.resolve(process.cwd(), '.env'));
+console.log(process.env);
 
 export const config = {
   images: {
